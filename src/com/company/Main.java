c5edb18ab0c9546b9a5d758b82381a620cd303fd
Olha Shevel <o.v.shevel@gmail.com>
2021-05-03 15:36:49 +0300

    Added measure distance func

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+    String data = "";
+        try {
+            File myObj = new File(/*"/Users/user/Desktop/Geosearch/src/com/company/ukraine_poi.csv"*/);
+            Scanner myReader = new Scanner(myObj);
+            if (myReader.hasNextLine()) {
+               data = myReader.nextLine();
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        Point d = new Point (data);
+        System.out.println(d.measureDistance(50.44075, 30.55263 ));
 	// write your code here
     }
+
+
+
 }
